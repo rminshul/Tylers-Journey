a4199c51f9c4faf22a9f484180b5b31995139cd4
SDBurke382 <teamburke88@gmail.com>
2022-11-14 11:49:57 -0500

    Transition Animations

---
+++ Tylers Journey/Assets/Scripts/TransitionHandler.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TransitionHandler : MonoBehaviour
+{
+    private string nextScene = "";
+    private bool disableFadeInAnimation = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (disableFadeInAnimation)
+        {
+            // Move fade-in animation to the end
+            Animator animator = gameObject.GetComponent<Animator>();
+            animator.Play("FadeIn", 0, 5);
+        }
+    }
+
+    void FadeOutFinished()
+    {
+        // Load of the next scene
+        SceneManager.LoadScene(nextScene);
+    }
+}
